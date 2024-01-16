5ecf22a6bd9f94b466735ce4848662650543c0df
sehbanalam <sehban.alam@gmail.com>
2024-01-17 00:48:12 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { Db, MongoClient, ServerApiVersion } from "mongodb";
+const app = express();
+const port = 3000;
+let db = undefined;
+// Replace the uri string with your connection string.
+const uri =
+  "mongodb+srv://restaurantapi:restaurant@cluster0.dv6p0ru.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    db = await client.db("restaurant_directory");
+    const restaurants = db.collection("restaurants");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  let all = db
+    .collection("restaurants")
+    .findOne({ name: "Gulbahar Chicken Fry" });
+  res.send(all);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
