391008d5f269713cd46029f7ce2427075b4b99e9
sehbanalam <sehban.alam@gmail.com>
2024-01-19 23:14:28 +0530

    vercel try

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+const app = express();
+const port = 3000;
+
+MongoClient.connect(process.env.MONGODB_URI)
+  .then(async (client) => {
+    app.locals.db = await client.db("restaurant_directory");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", async (req, res) => {
+  const db = req.app.locals.db;
+  let restaurants = [];
+  let cursor = await db.collection("restaurants").find();
+  for await (const doc of cursor) {
+    restaurants.push(doc);
+  }
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.json(restaurants);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
