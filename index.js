fa95b6aeb0ab6c5831b2af91f6d48b666ccd34a8
sehbanalam <sehban.alam@gmail.com>
2024-01-19 23:38:27 +0530

    vercel try 7

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ MongoClient.connect(process.env.MONGODB_URI)
     console.log(err);
   });
 
-app.get("/api/restaurants", async (req, res) => {
+app.get("/", async (req, res) => {
   const db = req.app.locals.db;
   let restaurants = [];
   let cursor = await db.collection("restaurants").find();
